44631c798c1a71c3eac81818c1664568560c6ffb
Andy <19520383+LeCyberDucky@users.noreply.github.com>
2020-07-26 19:52:31 +0200

    Haven't worked on this in a while. Not sure about changes. :)

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// https://guide.nannou.cc/tutorials/basics/drawing-2d-shapes.html
+
+use nannou::prelude::*;
+
+struct Model {}
+
+fn main() {
+    nannou::app(model)
+        .event(event)
+        .simple_window(view)
+        .run();
+}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {
+}
+
+fn view(_app: &App, _model: &Model, _frame: Frame) {
+}
